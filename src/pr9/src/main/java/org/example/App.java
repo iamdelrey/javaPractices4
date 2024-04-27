ab5ad9ea933bf940aa6163219df501b1f9d698a8
iamdelrey <stepanton94@mail.ru>
2024-04-28 02:30:42 +0300

    init

---
+++ src/pr9/src/main/java/org/example/App.java
@@ -0,0 +1,7 @@
+package pr9.src.main.java.org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world from Docker container");
+    }
+}
